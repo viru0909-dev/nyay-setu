732c4e37e585f0ad27eb712c8731007830e014dd
viru0909-dev <gadekarvirendra4@gmail.com>
2025-09-05 13:32:14 +0530

    adding module for or backend.

---
+++ nyay-setu-backend/src/main/java/com/nyaysetu/nyaysetubackend/NyaySetuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nyaysetu.nyaysetubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaySetuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyaySetuBackendApplication.class, args);
+    }
+
+}
